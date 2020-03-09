e0a378ac579a319f52b2a430df920d4d284f9be5
Earlopain <earlopain@c5h8no4na.net>
2020-03-09 18:11:27 +0000

    Initial commit

---
+++ e6nghelper.user.js
@@ -0,0 +1,314 @@
+// ==UserScript==
+// @name         E6NG Helper
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Remake of the now defunct eSix Extend
+// @author       Earlopain
+// @match        https://e621.net/*
+// @match        https://e926.net/*
+// @exclude      *.json
+// @grant GM_setValue
+// @grant GM_getValue
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    if (locationCheck("/posts/")) {
+        setTitle();
+        moveBottomNotice();
+        showUploader();
+    }
+    if (locationCheck("/uploads/new")) {
+        enhancePostUploader();
+    }
+    insertDtextFormatting();
+    insertCss();
+})();
+
+function setTitle() {
+    const oldTitle = document.title;
+    let prefix = "";
+    if (document.querySelector("#add-to-favorites").style.display === "none") {
+        prefix += "\u2665 "; //heart symbol
+    }
+    if (document.querySelector("#post-vote-up-" + document.location.href.split("/").pop().split("?")[0]).classList.contains("score-positive")) {
+        prefix += "\u2191 "; //arrow up
+    }
+    document.title = prefix + oldTitle;
+}
+
+function moveBottomNotice() {
+    let a = document.querySelector(".bottom-notices");
+    let sidebar = document.querySelector("#sidebar");
+    sidebar.insertBefore(a, sidebar.children[2]);
+    let clickThis = document.querySelector("#has-parent-relationship-preview-link");
+    if (clickThis) {
+        clickThis.click();
+    }
+    let clickThisToo = document.querySelector("#has-children-relationship-preview-link");
+    if (clickThisToo) {
+        clickThisToo.click();
+    }
+}
+
+function showUploader() {
+    const uploaderName = document.getElementById("image-container").getAttribute("data-uploader");
+    const uploaderId = document.getElementById("image-container").getAttribute("data-uploader-id");
+    const ul = document.getElementById("post-information").querySelector("ul");
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.href = "https://e621.net/users/" + uploaderId;
+    a.innerText = "Uploader: " + uploaderName;
+    li.appendChild(a);
+    ul.appendChild(li);
+}
+
+function insertDtextFormatting() {
+    const dtext = document.querySelectorAll(".dtext-previewable");
+    const buttons = [
+        { element: "strong", text: "Bold", insert: "[b]$selection[/b]" }, { element: "em", text: "Italics", insert: "[i]$selection[/i]" },
+        { element: "u", text: "Under", insert: "[u]$selection[/u]" }, { element: "s", text: "Strike", insert: "[s]$selection[/s]" },
+        { element: "span", text: "Spoiler", insert: "[spoiler]$selection[/spoiler]" },
+        { element: "span", text: "Link", insert: "\"\Link\":$selection" }, { element: "span", text: "Quote", insert: "[quote]$selection[/quote]" },
+        { element: "span", text: "Section", insert: "[section=Title]$selection[/section]" }
+    ];
+    //Steal the styles from a button. If you append buttons before the textare they
+    //somehow behave as if you clicked on send.
+    const templateButton = document.createElement("button");
+    //You can only get computed style on firefox if you append the element before
+    document.body.appendChild(templateButton);
+    const buttonStyleTemplate = getComputedStyle(templateButton);
+    templateButton.remove();
+    const buttonsPerRow = 4;
+    for (const preview of dtext) {
+        const textarea = preview.querySelector("textarea");
+        for (let i = 0; i < buttons.length; i++) {
+            if (i % buttonsPerRow === 0 && i !== 0) {
+                preview.insertBefore(document.createElement("br"), textarea);
+            }
+            const button = buttons[i];
+            const buttonElement = document.createElement("div");
+            buttonElement.classList.add("dtext-format-button");
+            const buttonText = document.createElement(button.element);
+            buttonText.innerText = button.text;
+            buttonElement.appendChild(buttonText);
+            buttonElement.style.cssText = buttonStyleTemplate;
+            buttonElement.addEventListener("click", () => {
+                const split = button.insert.split("$selection");
+                let insertStart = split[0];
+                let insertEnd = split[1];
+                if (textarea.selectionStart || textarea.selectionStart == '0') {
+                    const startPos = textarea.selectionStart;
+                    const endPos = textarea.selectionEnd;
+                    const selectedText = textarea.value.substring(startPos, endPos);
+                    textarea.value = textarea.value.substring(0, startPos)
+                        + insertStart + selectedText + insertEnd
+                        + textarea.value.substring(endPos, textarea.value.length);
+                } else {
+                    textarea.value += insertStart + insertEnd;
+                }
+            });
+            preview.insertBefore(buttonElement, textarea);
+        }
+    }
+}
+
+function enhancePostUploader() {
+    const textarea = document.getElementById("post_tags");
+    const appendTo = document.querySelector(".related-tag-functions");
+
+    const tagInput = document.createElement("input");
+    tagInput.classList.add("upload-post-button");
+    tagInput.spellcheck = false;
+    const tagCheckButton = document.createElement("button");
+    const tagInsertButton = document.createElement("button");
+    const sortTagsButton = document.createElement("button");
+    const infoText = document.createElement("div");
+    infoText.classList.add("tag-status-div");
+
+    let tagAlreadyChecked = false;
+
+    tagInput.addEventListener("keyup", event => {
+        if (event.keyCode !== 13) { //Enter
+            return
+        }
+        if (tagAlreadyChecked) {
+            tagInsertButton.click();
+        } else {
+            tagCheckButton.click();
+        }
+    });
+
+    tagInput.addEventListener("input", () => {
+        tagAlreadyChecked = false;
+    })
+
+    tagCheckButton.innerText = "Check";
+    tagCheckButton.classList.add("upload-post-button");
+    let previousTagText;
+    tagCheckButton.addEventListener("click", async () => {
+        const currentTag = prepareTagInput(tagInput.value);
+
+        if (tagAlreadyExists(currentTag)) {
+            infoText.innerText = "Tag already added";
+            return;
+        }
+
+        if (tagAlreadyChecked || currentTag === "") {
+            return;
+        }
+
+        previousTagText = currentTag;
+        const tagInfo = await getTagInfo(currentTag, infoText);
+        if (tagInfo.invalid) {
+            infoText.innerText = "Invalid tagname";
+            return;
+        }
+        tagAlreadyChecked = true;
+        infoText.innerText = tagInfo.count + " posts";
+        if (tagInfo.is_alias) {
+            infoText.innerText += " " + currentTag + " is alias of " + tagInfo.true_name;
+            tagInput.value = tagInfo.true_name;
+            previousTagText = tagInfo.true_name;
+        }
+    });
+
+    tagInsertButton.innerText = "Insert";
+    tagInsertButton.classList.add("upload-post-button");
+
+    tagInsertButton.addEventListener("click", () => {
+        tagAlreadyChecked = false;
+        const tag = prepareTagInput(tagInput.value);
+        tagInput.value = "";
+        const prefix = textarea.value.endsWith(" ") ? "" : " ";
+
+        textarea.value += prefix + tag.toLowerCase();
+        sortTagsButton.click();
+        infoText.innerText = "";
+    });
+
+    sortTagsButton.innerText = "Sort";
+    sortTagsButton.classList.add("upload-post-button");
+
+    sortTagsButton.addEventListener("click", () => {
+        const currentText = prepareInput(textarea.value);
+        const tags = currentText.split(" ");
+        tags.sort();
+        textarea.value = tags.join(" ");
+    });
+    appendTo.appendChild(document.createElement("br"));
+    appendTo.appendChild(tagInput);
+    appendTo.appendChild(tagCheckButton);
+    appendTo.appendChild(tagInsertButton);
+    appendTo.appendChild(sortTagsButton);
+    appendTo.appendChild(document.createElement("br"));
+    appendTo.appendChild(infoText);
+
+    function tagAlreadyExists(tag) {
+        const currentText = prepareInput(textarea.value);
+        const tags = currentText.split(" ");
+        return tags.indexOf(tag) !== -1;
+    }
+
+}
+
+function prepareInput(input) {
+    return input.trim().toLowerCase();
+}
+
+function prepareTagInput(input) {
+    return input.trim().toLowerCase().replace(/ /g, "_");
+}
+
+async function getTagInfo(tag, infoElement) {
+    tag = encodeURIComponent(tag);
+    const result = {
+        count: 0,
+        invalid: false,
+        is_alias: false,
+        true_name: undefined
+    }
+
+    let tagJson = JSON.parse(await getUrl("https://e621.net/tags/" + tag + ".json"));
+    if (tagJson === null) {
+        result.invalid = true;
+        return result;
+    } else if (tagJson.post_count !== 0) {
+        result.count = tagJson.post_count;
+        return result;
+    }
+    if (infoElement) {
+        infoElement.innerText = "Checking alias...";
+    }
+    const aliasJson = JSON.parse(await getUrl("https://e621.net/tag_aliases.json?search[antecedent_name]=" + tag));
+    if (aliasJson[0] === undefined) {
+        return result;
+    }
+    result.is_alias = true;
+    const trueTagName = aliasJson[0].consequent_name;
+    tagJson = JSON.parse(await getUrl("https://e621.net/tags/" + encodeURIComponent(trueTagName) + ".json"));
+    result.count = tagJson.post_count;
+    result.true_name = trueTagName;
+    return result;
+}
+
+function locationCheck(location) {
+    const domain = document.location.protocol + "//" + document.location.host;
+    return document.location.href.startsWith(domain + location);
+}
+
+function insertCss() {
+    const css = document.createElement("style");
+    css.innerHTML = `
+.dtext-format-button {
+    width: 50px !important;
+    height: 100% !important;
+    margin-right: 5px !important;
+    margin-bottom: 5px !important;
+}
+
+.dtext-format-button:hover {
+    background-color: rgb(34, 65, 115) !important;
+}
+
+.upload-post-button {
+    margin-left: 5px;
+    margin-top: 5px;
+}
+
+.tag-status-div {
+    margin-top: 5px;
+    height: 8px;
+}
+`;
+    document.head.appendChild(css);
+}
+
+function getComputedStyle(element) {
+    const styles = window.getComputedStyle(element);
+    if (styles.cssText !== '') {
+        return styles.cssText;
+    } else { //Fix for firefox
+        const cssText = Object.values(styles).reduce(
+            (css, propertyName) =>
+                `${css}${propertyName}:${styles.getPropertyValue(
+                    propertyName
+                )};`
+        );
+        return cssText;
+    }
+}
+
+async function getUrl(url) {
+    return new Promise((resolve, reject) => {
+        let request = new XMLHttpRequest();
+        request.open("GET", url, true);
+        request.addEventListener("load", () => {
+            if (request.status >= 200 && request.status < 400) {
+                resolve(request.responseText);
+            } else { reject(); }
+        });
+        request.addEventListener("error", () => { reject() });
+        request.send();
+    })
+}
